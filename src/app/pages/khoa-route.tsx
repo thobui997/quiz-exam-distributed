4f018c80c6564344fdfc5aa2dbd62ad28d8a1b85
tho.bui <thobui97@outlook.com>
2025-11-16 11:59:15 +0700

    remove route unused

--- src/app/pages/khoa-route.tsx
+++
@@ -1,7 +0,0 @@
-import KhoaList from '@app/features/khoa/components/khoa-list';
-
-const KhoaRoute = () => {
-  return <KhoaList />;
-};
-
-export default KhoaRoute;
--- src/app/pages/lop-route.tsx
+++
@@ -1,7 +0,0 @@
-import LopList from '@app/features/lop/components/lop-list';
-
-const LopRoute = () => {
-  return <LopList />;
-};
-
-export default LopRoute;
